a1996c81a5f23278b8f562467f2c7e6ccb461502
U-pou-PC\pou <pou@pou-PC.(none)>
2014-08-02 15:22:44 +0800

    aqaaa

---
+++ 189_select/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace _189_select
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "点击选择原文件地址";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "点击选择目标地址";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "xml原文件";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "xml目标文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "应用宝北京电信联系人导出";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 189_select/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace _189_select
+{
+
+       public partial class Form1 : Form
+    {
+
+    public class phone
+    {
+        public string phoneName;
+        public string phoneNum;
+    }
+        string fName;
+        string sName;
+        List<phone> flist = new List<phone>();
+        List<phone> slist = new List<phone>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (fName == null || sName == null)
+                MessageBox.Show("未选择完整");
+            ///读入xml文件
+            StreamReader sr=new StreamReader (fName);
+            string sfile = sr.ReadToEnd();
+            sr.Close();
+            int begin = 0;
+            int end = 0;
+            int nameIndex=0;
+            int nameIndexEnd=0;
+
+            ///转换xml文件
+            //while (begin <sfile.Length)
+            //{
+            //begin = sfile.IndexOf("<Name>",begin);
+            //    if (begin <0)
+            //        break;
+            //end = sfile.IndexOf("</Name>",begin);
+            //phoneName = sfile.Substring(begin+6, end-begin-6);
+            //if (phoneName != null||phoneName !="")
+            //{
+            //    flist.Add(phoneName);
+            //}
+            //begin=end;
+            //}
+
+            ///取出来phoneNum
+            while (end < sfile.Length)
+            {
+                end = sfile.IndexOf("</Phone>", begin);
+                if (end < 0)
+                    break;
+                begin = sfile.IndexOf(">", end-20);
+                phone myPhone = new phone();
+                myPhone.phoneNum = sfile.Substring(begin+1, end-begin-1);
+                //直接去除非数字
+                myPhone.phoneNum = System.Text.RegularExpressions.Regex.Replace(myPhone.phoneNum, @"[^\d]*", "");
+                ///判断+86/-/000
+                if (myPhone.phoneNum.IndexOf("86", 0) > 0 && myPhone.phoneNum.IndexOf("86", 0)<3)
+                {
+                    int head = myPhone.phoneNum.IndexOf("86", 0);
+                    myPhone.phoneNum = myPhone.phoneNum.Substring(head+2);
+                }
+                if (myPhone.phoneNum.IndexOf("-", 0) > 0)
+                {
+                    myPhone.phoneNum = myPhone.phoneNum.Replace("-", "");
+                }
+                   if (myPhone.phoneNum != null || myPhone.phoneNum != "")
+                {
+                    if (begin > 400)
+                    {
+                        nameIndexEnd = sfile.IndexOf("</Starred>", begin - 250);
+                        nameIndex = sfile.IndexOf("<Name>", nameIndexEnd - 70);
+                        myPhone.phoneName = sfile.Substring(nameIndex + 6, nameIndexEnd - nameIndex - 33);
+                        flist.Add(myPhone);
+                    }
+                    else
+                    {
+                        nameIndexEnd = sfile.IndexOf("</Starred>", begin - 170);
+                        nameIndex = sfile.IndexOf("<Name>", nameIndexEnd - 70);
+                        myPhone.phoneName = sfile.Substring(nameIndex + 6, nameIndexEnd - nameIndex - 33);
+                        flist.Add(myPhone);
+                    }
+                }
+              begin = end+1;
+            }
+
+            for (int i = 0; i < flist.Count; i++)
+            {
+                if (ok189(flist[i].phoneNum,1330100,1330139))
+                    slist.Add (flist[i]);
+                if (ok189(flist[i].phoneNum, 1331100, 1331140))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1331142, 1331159))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1332100, 1332106))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1332108, 1332119))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1333100, 1333119))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1334100, 1334119))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1335740, 1335749))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1336600, 1336699))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1337010, 1337018))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1337160, 1337179))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1338100, 1338149))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1339150, 1338199))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1530000, 1530039))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1530100, 1530139))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1531100, 1531199))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1531300, 1531399))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1532100, 1532199))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1533000, 1533029))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1534000, 1534019))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1890010, 1890019))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1890100, 1890139))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1891000, 1891199))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1800100, 1800139))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1801000, 1801049))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1802000, 1803999))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1804650, 1804659))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1805000, 1809999))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1810000, 1810004))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1810100, 1810139))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1811000, 1813999))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1814450, 1814459))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1814650, 1814659))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1770010, 1770019))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1770100, 1770139))
+                    slist.Add(flist[i]);
+                if (ok189(flist[i].phoneNum, 1771000, 1771019))
+                    slist.Add(flist[i]);
+
+            }
+            ///189号找全，再找名字
+
+//            拼接目标文件
+            if (slist.Count > 0)
+            {
+                string nameBlock = sfile.Substring(0, 50);
+                for (int i = 0; i < slist.Count; i++)
+                {
+                    nameBlock += sfile.Substring(50, 31) + slist[i].phoneName + "</Name>" + Environment.NewLine + "        <Starred>0</Starred>" + Environment.NewLine + "        <PhoneList>" + Environment.NewLine + "            <Phone Type=\"2\">" + slist[i].phoneNum + "</Phone>" + Environment.NewLine + "        </PhoneList>" + Environment.NewLine + "        <Account value=\"0\">" + Environment.NewLine + "            <Name></Name>" +Environment.NewLine+ "            <Type></Type>" + Environment.NewLine + "        </Account>" + Environment.NewLine + "    </Contact>";
+                }
+                nameBlock += Environment.NewLine + "</Contacts>";
+                ///写入xml文件
+                FileStream fs = new FileStream(sName, FileMode.Append, FileAccess.Write);
+                fs.Close();
+                StreamWriter sw = File.AppendText(sName);
+                sw.Write(nameBlock);
+                sw.Flush();
+                sw.Close();
+                MessageBox.Show("写入成功");
+            }
+            else
+                MessageBox.Show("没有找到北京电信的号码，未生成文件");
+        }
+           public static bool ok189(string num,int first,int last)
+           {
+               //如果长度小于7，返回false
+               if (num.Length < 7)
+                   return false;
+               int tem=int.Parse(num.Substring(0, 7).ToString());
+                if (first <= int.Parse(num.Substring(0, 7)) && int.Parse(num.Substring(0, 7)) <= last)
+                {
+                    return true;
+                }
+                else
+                    return false;
+           }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "选择原文件";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            ofd.Filter = "xml|*.xml";
+            ofd.RestoreDirectory = true;
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fName = ofd.FileName;
+                textBox1.Text = fName;
+            }
+
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "xml|*.xml";
+            sfd.Title = "保存地址";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                sName = sfd.FileName;
+                textBox2.Text = sName;
+            }
+        }
+    }
+}
